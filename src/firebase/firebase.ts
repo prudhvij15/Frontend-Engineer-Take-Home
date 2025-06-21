30d7c736b05aef3ed80c5167fc5e6187317469e8
prudhvij15 <jampanaprudhvi2@gmail.com>
2025-06-21 17:40:22 -0500

    removed console logs from firebase setup

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -14,6 +14,6 @@ const firebaseConfig = {
 };
  
 console.log("Firebase config loaded");
-console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app); 
